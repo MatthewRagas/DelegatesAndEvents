18a1fdd5acec9ca1b8237bd34f02b6a09c245670
Matthew John Ragas <mjrag310@hotmail.com>
2019-09-16 12:57:10 -0500

    Add project files.

---
+++ DelegatesAndEvents/ArrayList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesAndEvents
+{
+    class ArrayList
+    {
+        private int[] _numList = new int[4];
+
+        public ArrayList()
+        {
+            _numList[0] = 1;
+            _numList[1] = 2;
+            _numList[2] = 3;
+            _numList[3] = 4;
+        }
+
+        public int[] Add(int[] array, int value)
+        {
+           _numList = new int[array.Length + 1];         
+
+           for(int i = 0; i < array.Length; i++)
+            {
+                _numList[i] = array[i];
+            }
+
+            _numList[array.Length] = value;
+
+            for(int i = 0; i < _numList.Length; i++)
+            {
+                Console.WriteLine(_numList[i]);
+            }
+
+            return _numList;
+        }
+    }
+}
---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            int[] arrayList = new int[4];            
+            arrayList[0] = 1;
+            arrayList[1] = 2;
+            arrayList[2] = 3;
+            arrayList[3] = 4;
+            
+
+            for (int i = 0; i < arrayList.Length; i++)
+            {
+                Console.WriteLine(arrayList[i]);
+            }
+
+            Console.ReadKey();
+            arrayList = list.Add(arrayList, 5);
+            Console.ReadKey();
+            arrayList = list.Add(arrayList, 6);
+            Console.ReadKey();
+            arrayList = list.Add(arrayList, 99);
+
+            //for (int i = 0; i < arrayList.Length; i++)
+            //{
+            //    Console.WriteLine(arrayList[i]);
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
